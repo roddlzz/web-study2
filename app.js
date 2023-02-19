8698c2a7eb96cdaebad53122869185f168755671
roddlzz <2770121@naver.com>
2023-02-19 16:49:04 +0900

    230219

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const helmet = require("helmet");
 const app = express();
 const ejs = require("ejs")
 const db = require('./model/db')
+const json2xls = require('json2xls');
 
 app.set('view engine', 'ejs');
 app.set('views', './views')
 app.use('/public',express.static(__dirname + '/public'));
-
 // app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(json2xls.middleware);
 
 const mainRouter = require('./router/mainRouter')
 app.use('/',mainRouter)
-app.listen(3000, function(req,res){
-    db.sequelize.sync({ force: false })
+
+app.listen(3000,function(req,res){
+
+    db.sequelize.sync({force:false})
     console.log("서버 실행")
 })
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -2,11 +2,39 @@ const express = require('express')
 const router = express.Router();
 const db = require('../model/db');
 
+const cheerio = require("cheerio")
+const axios = require("axios")
+const iconv = require("iconv-lite")
+const url = "https://finance.naver.com/sise/lastsearch2.nhn"
+
+
+router.get("/excel/down", function(req,res){
+    let excel_data = [{"A":1,"B":2,"C":3,"D":4}]
+    res.xls('data.xlsx', excel_data);
+})
+
+router.get("/excel", function(req,res){
+    res.render("excel")
+})
+
+router.get("/crawling", function(req,res){
+
+    axios({url:url,method:"GET",responseType:"arraybuffer"}).then(function(html){
+        const content = iconv.decode(html.data,"EUC-KR").toString()
+        const $ = cheerio.load(content)
+
+        const table = $(".type_5 tr td")
+        table.each(function(i,tag){
+            console.log($(tag).text().trim())
+        })
+
+        res.send({success:200})
+    })
+})
+
 router.get("/", function(req,res){
     res.render('main',{title:"영화 리뷰 사이트"})
 })
-
-
 
 router.post("/review/create", function(req,res){
     let movie_id = req.body.movie_id;
